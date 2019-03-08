a4b995b4f6598614b640ef7f6c947cece4ac6864
J X <jw@Js-MacBook-Pro.local>
2019-03-07 19:32:37 -0800

    put no13 aside for now

--- warm-up/lint_no13_ImplementingStrstr.py
+++ warm-up/lint_no13_ImplementingStrstr.py
@@ -20,6 +20,11 @@ class Solution:
         Input: source = "source", target = "sourcez"
     test 2:
         Input: source = "source", target = "source"
+    first submission:
+        cannot pass
+        Input: source = "tartarget", target = "target"
+        expected: 3
+        real: -1
 
     """
     def strStr(self, source, target):
@@ -28,7 +33,11 @@ class Solution:
         i_source = 0
         i_target = 0
         first_ocr_index = -1
+        if not target:
+            return 0
+        #while 3 < 9 - 6 + 1; source[3] == target[0], first_ocr_index = 3.
         while i_source < len(source) - len(target) + 1:
+            print(i_source)
             if source[i_source] == target[0]:
                 first_ocr_index = i_source
 
@@ -39,12 +48,18 @@ class Solution:
                     elif source[i_source] == target[i_target]:
                         i_target += 1
                         i_source += 1
+                    #corner case: when new i_source cannot keep aligning, but could possibly start a new alignment.
+                    elif source[i_source] == target[0]:
+                        first_ocr_index = i_source
+                        continue
                     else:
                         first_ocr_index = -1
                         i_source += 1
-                        continue
+                        break
             else:
                 i_source += 1
                     
         return first_ocr_index
+
+# this one has not pass the test yet...
             
